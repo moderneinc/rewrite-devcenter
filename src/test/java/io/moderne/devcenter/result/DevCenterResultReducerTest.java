86ec1ca60d33f8daa7c89774eb5844fb5f8f8192
Jonathan Schneider <jkschneider@gmail.com>
2025-06-14 18:04:59 -0400

    Tests for empty data tables

--- src/test/java/io/moderne/devcenter/result/DevCenterResultReducerTest.java
+++ src/test/java/io/moderne/devcenter/result/DevCenterResultReducerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.openrewrite.config.Environment;
 
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -16,17 +17,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
 public class DevCenterResultReducerTest {
+    Environment environment = Environment.builder()
+      .scanRuntimeClasspath("org.openrewrite")
+      .scanYamlResources()
+      .build();
+    DevCenter devCenter = new DevCenter(environment.activateRecipes("io.moderne.devcenter.DevCenterStarter"));
+    Organization<Object> root = new OrganizationReader().fromCsv(requireNonNull(getClass()
+      .getResourceAsStream("/repos-Default.csv")));
 
     @Test
-    void results() {
-        Environment environment = Environment.builder()
-          .scanRuntimeClasspath("org.openrewrite")
-          .scanYamlResources()
-          .build();
-        DevCenter devCenter = new DevCenter(environment.activateRecipes("io.moderne.devcenter.DevCenterStarter"));
-        Organization<Object> root = new OrganizationReader().fromCsv(requireNonNull(getClass()
-          .getResourceAsStream("/repos-Default.csv")));
-
+    void upgradesAndSecurityIssues() {
         DevCenterResultReducer reducer = DevCenterResultReducer.fromDataTables(
           devCenter,
           root,
@@ -40,23 +40,56 @@ public class DevCenterResultReducerTest {
         root.forEachOrganization(org -> {
             orgsChecked.add(org.getName());
             DevCenterResult result = reducer.reduce(org);
-
-            result.forEach(devCenter.getCard("Move to Spring Boot 3.5.0"), (measure, count) -> {
-                assertThat(count)
-                  .describedAs(measure.getName())
-                  .isEqualTo((int) switch (measure) {
-                      case SemverMeasure.Minor -> 3;
-                      case SemverMeasure.Completed -> 1;
-                      default -> fail("Unexpected measure");
-                  });
-            });
-
-            result.forEach(devCenter.getSecurity(), (measure, count) -> {
-                assertThat(measure.getName()).isEqualTo("Remediate OWASP A08:2021 Software and data integrity failures");
-                assertThat(count).isEqualTo(20);
-            });
+            hasSpringBoot35Results(result);
+            hasSecurityResults(result);
         });
 
         assertThat(orgsChecked).containsExactly("ALL", "Default", "ε");
+    }
+
+    @Test
+    void emptyDataTable() {
+        DevCenterResultReducer reducer = DevCenterResultReducer.fromDataTables(
+          devCenter,
+          root,
+          new StringReader(""),
+          new StringReader("")
+        );
+
+        assertThat(reducer.reduce(root).getResultsByCard()).isEmpty();
+    }
+
+    @Test
+    void emptySecurity() {
+        DevCenterResultReducer reducer = DevCenterResultReducer.fromDataTables(
+          devCenter,
+          root,
+          new InputStreamReader(requireNonNull(getClass().getResourceAsStream(
+            "/UpgradesAndMigrations-Default.csv"))),
+          new StringReader("")
+        );
+
+        assertThat(reducer.reduce(root).getResultsByCard().keySet())
+          .map(DevCenter.Card::getName)
+          .containsExactly("Move to Spring Boot 3.5.0");
+    }
+
+    private void hasSecurityResults(DevCenterResult result) {
+        result.forEach(devCenter.getSecurity(), (measure, count) -> {
+            assertThat(measure.getName()).isEqualTo("Remediate OWASP A08:2021 Software and data integrity failures");
+            assertThat(count).isEqualTo(20);
+        });
+    }
+
+    private void hasSpringBoot35Results(DevCenterResult result) {
+        result.forEach(devCenter.getCard("Move to Spring Boot 3.5.0"), (measure, count) -> {
+            assertThat(count)
+              .describedAs(measure.getName())
+              .isEqualTo((int) switch (measure) {
+                  case SemverMeasure.Minor -> 3;
+                  case SemverMeasure.Completed -> 1;
+                  default -> fail("Unexpected measure");
+              });
+        });
     }
 }
