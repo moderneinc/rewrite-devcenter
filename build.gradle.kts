31e66e0fbf7be397f8968d06c0e03cf40e10eaeb
Laurens Westerlaken <laurens.w@live.nl>
2025-07-02 11:28:51 +0000

    refactor: org.openrewrite.mavencentral
    Use this link to re-run the recipe: https://app.moderne.io/builder/8upHD4c2r?organizationId=QUxML01vZGVybmU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,6 @@ dependencies {
 nexusPublishing {
     repositories.getByName("sonatype") {
         nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-        snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
     }
 }
