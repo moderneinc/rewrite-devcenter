0d1fa66d78e69cee38f6419d7f31865d5b4c256e
Laurens Westerlaken <laurens.w@live.nl>
2025-07-02 13:39:54 -0400

    refactor: org.openrewrite.mavencentral (#24)
    * refactor: org.openrewrite.mavencentral
    
    Use this link to re-run the recipe: https://app.moderne.io/builder/8upHD4c2r?organizationId=QUxML01vZGVybmU%3D
    
    Co-authored-by: Moderne <team@moderne.io>
    
    * Update gradle task for publishing to maven central
    
    ---------
    
    Co-authored-by: Moderne <team@moderne.io>
    Co-authored-by: Greg Oledzki <greg.oledzki@moderne.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
 
 nexusPublishing {
     repositories.getByName("sonatype") {
-        nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-        snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+        snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
     }
 }
