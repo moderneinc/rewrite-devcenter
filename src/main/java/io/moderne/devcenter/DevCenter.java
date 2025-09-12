e4e05f15ca33eba562bc5c51be8430f49d2cdbc3
Tim te Beek <tim@moderne.io>
2025-09-12 13:22:05 +0200

    Apply some of the best practices already

--- src/main/java/io/moderne/devcenter/DevCenter.java
+++ src/main/java/io/moderne/devcenter/DevCenter.java
@@ -26,7 +26,10 @@ import org.openrewrite.config.DataTableDescriptor;
 import org.openrewrite.config.OptionDescriptor;
 import org.openrewrite.config.RecipeDescriptor;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static java.util.Collections.unmodifiableList;
--- src/test/java/io/moderne/devcenter/DevCenterTest.java
+++ src/test/java/io/moderne/devcenter/DevCenterTest.java
@@ -22,6 +22,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.Recipe;
 import org.openrewrite.config.Environment;
 import org.openrewrite.config.YamlResourceLoader;
@@ -246,6 +247,7 @@ class DevCenterTest implements RewriteTest {
         );
     }
 
+    @DocumentExample
     @Test
     void devcenterWithMultipleLibraryUpgradeRecipesHasCorrectData() {
         @Language("yaml") String recipe = """
