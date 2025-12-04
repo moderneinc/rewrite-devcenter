a83b6029f88780e29d80771f54b8f38d8a88f7e0
Tim te Beek <tim@moderne.io>
2025-12-04 20:39:11 +0100

    Expect a SearchResult marker from DependencyInsight

--- src/test/java/io/moderne/devcenter/LibraryUpgradeTest.java
+++ src/test/java/io/moderne/devcenter/LibraryUpgradeTest.java
@@ -74,14 +74,14 @@ class LibraryUpgradeTest implements RewriteTest {
                 <artifactId>example</artifactId>
                 <version>1.0-SNAPSHOT</version>
                 <dependencies>
-                    <!--~~>--><dependency>
+                    <!--~~(com.fasterxml.jackson.core:jackson-annotations:%1$s,com.fasterxml.jackson.core:jackson-core:%1$s,com.fasterxml.jackson.core:jackson-databind:%1$s,com.fasterxml.jackson.module:jackson-module-parameter-names:%1$s%2$s)~~>--><dependency>
                         <groupId>com.fasterxml.jackson.module</groupId>
                         <artifactId>jackson-module-parameter-names</artifactId>
-                        <version>%s</version>
+                        <version>%1$s</version>
                     </dependency>
                 </dependencies>
               </project>
-              """.formatted(currentVersion)
+              """.formatted(currentVersion, currentVersion.equals(targetVersion) ? "" : ",com.fasterxml.jackson:jackson-bom:" + currentVersion)
           )
         );
     }
