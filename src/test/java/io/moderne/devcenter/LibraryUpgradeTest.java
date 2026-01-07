4fc637c18d13d4351ca0f1bdb4547120d2251163
Tim te Beek <tim@moderne.io>
2026-01-07 18:43:45 +0100

    Drop `jackson-bom` following openrewrite/rewrite#6488

--- src/test/java/io/moderne/devcenter/LibraryUpgradeTest.java
+++ src/test/java/io/moderne/devcenter/LibraryUpgradeTest.java
@@ -77,7 +77,7 @@ class LibraryUpgradeTest implements RewriteTest {
                 <artifactId>example</artifactId>
                 <version>1.0-SNAPSHOT</version>
                 <dependencies>
-                    <!--~~(com.fasterxml.jackson.core:jackson-annotations:%1$s,com.fasterxml.jackson.core:jackson-core:%1$s,com.fasterxml.jackson.core:jackson-databind:%1$s,com.fasterxml.jackson.module:jackson-module-parameter-names:%1$s,com.fasterxml.jackson:jackson-bom:%1$s)~~>--><dependency>
+                    <!--~~(com.fasterxml.jackson.core:jackson-annotations:%1$s,com.fasterxml.jackson.core:jackson-core:%1$s,com.fasterxml.jackson.core:jackson-databind:%1$s,com.fasterxml.jackson.module:jackson-module-parameter-names:%1$s)~~>--><dependency>
                         <groupId>com.fasterxml.jackson.module</groupId>
                         <artifactId>jackson-module-parameter-names</artifactId>
                         <version>%1$s</version>
