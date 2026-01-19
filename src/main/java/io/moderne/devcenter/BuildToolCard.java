d473f4a584917ea7bc1c8c52a6edeebe35d6a33d
Tim te Beek <tim@moderne.io>
2026-01-19 19:11:53 +0100

    Order imports

--- src/main/java/io/moderne/devcenter/BuildToolCard.java
+++ src/main/java/io/moderne/devcenter/BuildToolCard.java
@@ -28,9 +28,7 @@ import org.openrewrite.marker.BuildTool;
 import org.openrewrite.semver.Semver;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.Collections.singletonList;
 
